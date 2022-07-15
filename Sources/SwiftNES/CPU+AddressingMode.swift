e7e3b580d9256c1c094756fb682a7cb4914b4e94
swiftty <62803132+swiftty@users.noreply.github.com>
2022-07-15 23:18:22 +0900

    [misc] add trace() method

--- Sources/SwiftNES/CPU+AddressingMode.swift
+++ Sources/SwiftNES/CPU+AddressingMode.swift
@@ -16,53 +16,59 @@ extension CPU {
 }
 
 extension CPU {
-    func operand_address(_ mode: AddressingMode) -> UInt16 {
+    func absolute_address(_ mode: AddressingMode, _ addr: UInt16) -> UInt16 {
         switch mode {
-        case .immediate:
-            return programCounter
-
         case .zeroPage:
-            return UInt16(mem_read(programCounter))
+            return UInt16(mem_read(addr))
 
         case .zeroPageX:
-            let pos = mem_read(programCounter)
+            let pos = mem_read(addr)
             let addr = pos &+ registerX
             return UInt16(addr)
 
         case .zeroPageY:
-            let pos = mem_read(programCounter)
+            let pos = mem_read(addr)
             let addr = pos &+ registerY
             return UInt16(addr)
 
         case .absolute:
-            return mem_read_16(programCounter)
+            return mem_read_16(addr)
 
         case .absoluteX:
-            let pos = mem_read_16(programCounter)
+            let pos = mem_read_16(addr)
             let addr = pos &+ UInt16(registerX)
             return addr
 
         case .absoluteY:
-            let pos = mem_read_16(programCounter)
+            let pos = mem_read_16(addr)
             let addr = pos &+ UInt16(registerY)
             return addr
 
         case .indirectX:
-            let base = mem_read(programCounter)
+            let base = mem_read(addr)
             let ptr = base &+ registerX
             let lo = UInt16(mem_read(UInt16(ptr)))
             let hi = UInt16(mem_read(UInt16(ptr &+ 1)))
             return UInt16(hi) << 8 | lo
 
         case .indirectY:
-            let base = mem_read(programCounter)
+            let base = mem_read(addr)
             let lo = UInt16(mem_read(UInt16(base)))
             let hi = UInt16(mem_read(UInt16(base &+ 1)))
             let deref_base = hi << 8 | lo
             return deref_base &+ UInt16(registerY)
 
-        case .none:
+        case .immediate, .none:
             fatalError("mode \(mode) is not supported")
+        }
+
+    }
+
+    func operand_address(_ mode: AddressingMode) -> UInt16 {
+        if mode == .immediate {
+            return programCounter
+        } else {
+            return absolute_address(mode, programCounter)
         }
     }
 }
--- Sources/SwiftNES/StringInterpolation+.swift
+++ Sources/SwiftNES/StringInterpolation+.swift
@@ -1,5 +1,106 @@
 import Foundation
 
+public func trace(_ cpu: CPU) -> String {
+    let code = cpu.mem_read(cpu.programCounter)
+    let ops = CPU.OpCode.codes[code]!
+
+    let begin = cpu.programCounter
+    var hex_dump = [code]
+
+    let (addr, value): (UInt16, UInt8) = {
+        switch ops.mode {
+        case .immediate, .none:
+            return (0, 0)
+        default:
+            let addr = cpu.absolute_address(ops.mode, begin + 1)
+            return (addr, cpu.mem_read(addr))
+        }
+    }()
+
+    let tmp: String = {
+        switch ops.count {
+        case 1:
+            switch code {
+            case 0x0a, 0x4a, 0x2a, 0x6a: return "A "
+            default: return ""
+            }
+
+        case 2:
+            let address = cpu.mem_read(begin + 1)
+            hex_dump.append(address)
+
+            switch ops.mode {
+            case .immediate:
+                return "#$\(hex: address)"
+            case .zeroPage:
+                return "$\(hex: addr) = \(hex: value)"
+            case .zeroPageX:
+                return "$\(hex: address),X @ \(hex: addr) = \(hex: value)"
+            case .zeroPageY:
+                return "$\(hex: address),Y @ \(hex: addr) = \(hex: value)"
+            case .indirectX:
+                return "($\(hex: address), X) @ \(hex: address &+ cpu.registerX) \(hex: addr) = \(hex: value)"
+            case .indirectY:
+                return "($\(hex: address), Y) @ \(hex: address &+ cpu.registerY) \(hex: addr) = \(hex: value)"
+            case .none:
+                let address = Int(begin + 2) &+ Int(Int8(address))
+                return "$\(hex: address)"
+            default:
+                fatalError()
+            }
+
+        case 3:
+            let lo = cpu.mem_read(begin + 1)
+            let hi = cpu.mem_read(begin + 2)
+            hex_dump.append(contentsOf: [lo, hi])
+
+            let address = cpu.mem_read_16(begin + 1)
+
+            switch ops.mode {
+            case .none:
+                if code == 0x6c {
+                    let addr: UInt16
+                    if (address & 0x00ff) == 0x00ff {
+                        let lo = UInt16(cpu.mem_read(address))
+                        let hi = UInt16(cpu.mem_read(address & 0xff00))
+                        addr = hi << 8 | lo
+                    } else {
+                        addr = cpu.mem_read_16(address)
+                    }
+                    return "($\(hex: address)) = \(hex: addr)"
+                } else {
+                    return "$\(hex: address)"
+                }
+            case .absolute:
+                return "$\(hex: addr) = \(hex: value)"
+            case .absoluteX:
+                return "$\(hex: address),X @ \(hex: addr) = \(hex: value)"
+            case .absoluteY:
+                return "$\(hex: address),Y @ \(hex: addr) = \(hex: value)"
+            default:
+                fatalError()
+            }
+
+        default:
+            return ""
+        }
+    }()
+
+    let hex_str = hex_dump
+        .map { "\(hex: $0)" }
+        .joined(separator: " ")
+    let asm_str = "\(hex: begin)  \(hex_str) \(ops.mnemonic) \(tmp)"
+
+    return [
+        "\(asm_str)",
+        "A:\(hex: cpu.registerA)",
+        "X:\(hex: cpu.registerX)",
+        "Y:\(hex: cpu.registerY)",
+        "P:\(hex: cpu.status.rawValue)",
+        "SP:\(hex: cpu.stackPointer)"
+    ].joined(separator: " ")
+}
+
 extension DefaultStringInterpolation {
     mutating func appendInterpolation(hex: some BinaryInteger) {
         var hex = String(hex, radix: 16, uppercase: false)
